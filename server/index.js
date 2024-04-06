ba248c8e8e263e2fb4084e4d839ccccf53bcd3ef
rishabhrawat526 <rishabhrawat526@gmail.com>
2024-04-06 16:05:28 +0530

    deployment

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ import Routes from './routes/route.js';
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://todo-app-theta-gules.vercel.app');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'https://todo-app-theta-gules.vercel.app');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     next();
+// });
 
 app.use(cors(
     {
@@ -20,7 +20,7 @@ app.use(cors(
         credentials:true
     }
 ));
-
+app.use(express.json());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }))
 
