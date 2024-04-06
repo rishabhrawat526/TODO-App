7f06c08fa6ebfd02eaf7d5b668f822ebf369eab5
rishabhrawat526 <rishabhrawat526@gmail.com>
2024-04-06 15:56:41 +0530

    deployment

--- server/index.js
+++ server/index.js
@@ -7,6 +7,11 @@ import Routes from './routes/route.js';
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 app.use(cors(
     {
