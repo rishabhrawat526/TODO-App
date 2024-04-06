fb28be1671b474ee633a294080c33000f41f9504
rishabhrawat526 <rishabhrawat526@gmail.com>
2024-04-06 12:38:01 +0530

    deployment

---
+++ server/database/db.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const Connection = () => {
+
+    const MONGODB_URI =process.env.DB_CONNECTION_STRING.replace('<password>',process.env.DATABASE_PASSWORD);
+
+    mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+    mongoose.connection.on('connected', () => {
+        console.log('Database connected Successfully');
+    })
+
+    mongoose.connection.on('disconnected', () => {
+        console.log('Database disconnected');
+    })
+
+    mongoose.connection.on('error', () => {
+        console.log('Error while connecting with the database ', error.message);
+    })
+}
+
+export default Connection;
