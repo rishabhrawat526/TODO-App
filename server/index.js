e1465d913f0ef180b362f916b619a93c4227e4c0
rishabhrawat526 <rishabhrawat526@gmail.com>
2024-04-06 16:03:36 +0530

    deployment

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import Routes from './routes/route.js';
 const app = express();
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://todo-app-theta-gules.vercel.app');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
